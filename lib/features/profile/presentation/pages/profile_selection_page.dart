f21b3fa3029bf88dedb419248b97af0cb0b055fd
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-08-22 21:29:25 +0530

    Profile Selection Page UI Completed

--- lib/features/profile/presentation/pages/profile_selection_page.dart
+++ lib/features/profile/presentation/pages/profile_selection_page.dart
@@ -1,3 +1,4 @@
+import 'package:aparna_education/core/widgets/project_button.dart';
 import 'package:aparna_education/features/auth/presentation/pages/landing_page.dart';
 import 'package:aparna_education/features/profile/presentation/widgets/profile_type_widget.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -14,6 +15,9 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  bool teacherIsSelected = false;
+  bool studentIsSelected = false;
+  bool languageLearnerIsSelected = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,10 +39,93 @@ class _HomePageState extends State<HomePage> {
       body: SingleChildScrollView(
         child: Padding(
           padding: const EdgeInsets.all(10.0),
-          child: Column(
+          child: Stack(
             children: [
-              const SizedBox(height: 20),
-              const ProfileTypeWidget(),
+              SizedBox(
+                height: MediaQuery.of(context).size.height,
+                child: Column(
+                  children: [
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        if (teacherIsSelected) {
+                          setState(() {
+                            teacherIsSelected = false;
+                          });
+                          return;
+                        }
+                        setState(() {
+                          teacherIsSelected = true;
+                          studentIsSelected = false;
+                          languageLearnerIsSelected = false;
+                        });
+                      },
+                      child: ProfileTypeWidget(
+                        isSelected: teacherIsSelected,
+                        profileType: ProfileType.teacher,
+                        imageUrl: 'assets/images/teacher.png',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        if (studentIsSelected) {
+                          setState(() {
+                            studentIsSelected = false;
+                          });
+                          return;
+                        }
+                        setState(() {
+                          studentIsSelected = true;
+                          teacherIsSelected = false;
+                          languageLearnerIsSelected = false;
+                        });
+                      },
+                      child: ProfileTypeWidget(
+                        isSelected: studentIsSelected,
+                        profileType: ProfileType.student,
+                        imageUrl: 'assets/images/student.png',
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    GestureDetector(
+                      onTap: () {
+                        if (languageLearnerIsSelected) {
+                          setState(() {
+                            languageLearnerIsSelected = false;
+                          });
+                          return;
+                        }
+                        setState(() {
+                          languageLearnerIsSelected = true;
+                          teacherIsSelected = false;
+                          studentIsSelected = false;
+                        });
+                      },
+                      child: ProfileTypeWidget(
+                        isSelected: languageLearnerIsSelected,
+                        profileType: ProfileType.languagelearner,
+                        imageUrl: 'assets/images/learner.png',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Positioned(
+                  top: MediaQuery.of(context).size.height * 0.8,
+                  left: MediaQuery.of(context).size.width * 0.2,
+                  right: MediaQuery.of(context).size.width * 0.2,
+                  child: ProjectButton(
+                      text: "Get Started",
+                      onPressed: !teacherIsSelected &&
+                              !studentIsSelected &&
+                              !languageLearnerIsSelected
+                          ? () {}:(){},
+                      isInverted: !teacherIsSelected &&
+                              !studentIsSelected &&
+                              !languageLearnerIsSelected
+                          ? true
+                          : false)),
             ],
           ),
         ),
--- lib/features/profile/presentation/widgets/profile_type_widget.dart
+++ lib/features/profile/presentation/widgets/profile_type_widget.dart
@@ -4,7 +4,14 @@ import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 class ProfileTypeWidget extends StatefulWidget {
-  const ProfileTypeWidget({super.key});
+  final bool isSelected;
+  final ProfileType profileType;
+  final String imageUrl;
+  const ProfileTypeWidget(
+      {super.key,
+      required this.profileType,
+      required this.imageUrl,
+      required this.isSelected});
 
   @override
   State<ProfileTypeWidget> createState() => _ProfileTypeWidgetState();
@@ -19,8 +26,21 @@ class _ProfileTypeWidgetState extends State<ProfileTypeWidget> {
       padding: const EdgeInsets.all(10),
       width: double.infinity,
       decoration: BoxDecoration(
-        border: Border.all(color: Pallete.inactiveColor, width: 3.5),
-        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        shape: BoxShape.rectangle,
+        border: Border.all(
+            color:
+                widget.isSelected ? Pallete.primaryColor : Pallete.whiteColor,
+            width: 3.5),
+        borderRadius: BorderRadius.circular(15),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 2,
+            blurRadius: 5,
+            offset: Offset(0, 3), // changes the position of the shadow
+          ),
+        ],
       ),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -28,7 +48,7 @@ class _ProfileTypeWidgetState extends State<ProfileTypeWidget> {
           ClipRRect(
             borderRadius: BorderRadius.circular(10),
             child: Image(
-              image: AssetImage('assets/images/teacher.png'),
+              image: AssetImage(widget.imageUrl),
               height: 130,
               width: 130,
               fit: BoxFit.cover,
@@ -37,19 +57,31 @@ class _ProfileTypeWidgetState extends State<ProfileTypeWidget> {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                'I\'m a Teacher',
-                style: TextStyle(
-                  fontSize: 23,
-                  fontWeight: FontWeight.bold,
-                  color: Pallete.secondaryColor,
+              SizedBox(
+                width: MediaQuery.of(context).size.width * 0.5,
+                child: Text(
+                  widget.profileType == ProfileType.teacher
+                      ? 'I\'m a Teacher'
+                      : widget.profileType == ProfileType.student
+                          ? 'I\'m a Student'
+                          : 'I\'m a Language Learner',
+                  style: const TextStyle(
+                    fontSize: 23,
+                    fontWeight: FontWeight.bold,
+                    color: Pallete.secondaryColor,
+                  ),
                 ),
               ),
               SizedBox(
                 width: MediaQuery.of(context).size.width * 0.5,
-                child: const Text(
-                  'I am a teacher and I want to teach students.',
-                  style: TextStyle(
+                child: Text(
+                  widget.profileType == ProfileType.teacher
+                      ? 'I am a teacher and I want to teach students.'
+                      : widget.profileType == ProfileType.student
+                          ? 'I am a student and I want to learn.'
+                          : 'I am a language learner and I want to learn languages.',
+                  maxLines: 2,
+                  style: const TextStyle(
                     fontSize: 15,
                     color: Colors.black,
                   ),
