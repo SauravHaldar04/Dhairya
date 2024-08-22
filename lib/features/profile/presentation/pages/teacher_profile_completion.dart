5f971cdb2ed15b5626a399eb669452690126e3cb
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-08-23 00:15:00 +0530

    Pick Image for profile photo in teacher profile

--- lib/features/profile/presentation/pages/teacher_profile_completion.dart
+++ lib/features/profile/presentation/pages/teacher_profile_completion.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:aparna_education/core/theme/app_pallete.dart';
+import 'package:aparna_education/core/utils/pickimage.dart';
 import 'package:aparna_education/core/widgets/project_textfield.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
@@ -13,6 +16,17 @@ class TeacherProfileCompletion extends StatefulWidget {
 
 class _TeacherProfileCompletionState extends State<TeacherProfileCompletion> {
   TextEditingController genderController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  File? image;
+  void selectImage() async {
+    final file = await pickImage();
+    if (file == null) return;
+    setState(() {
+      image = file;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,36 +44,48 @@ class _TeacherProfileCompletionState extends State<TeacherProfileCompletion> {
             child: Column(
               children: [
                 SizedBox(height: 20),
-                Stack(
-                  children: [
-                    Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        // border: Border.all(
-                        //   color: Pallete.primaryColor,
-                        //   width: 3.5,
-                        // ),
-                      ),
-                      height: MediaQuery.of(context).size.height * 0.3,
-                      width: MediaQuery.of(context).size.width,
-                      child: Image.asset(
-                        'assets/images/profileImagePlaceholder.png',
-                      ),
-                    ),
-                    Positioned(
-                      left: MediaQuery.of(context).size.width * 0.25,
-                      bottom: MediaQuery.of(context).size.height * 0.01,
-                      child: IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.add_a_photo,
-                          size: 40,
-                          color: Pallete.primaryColor,
+                image != null
+                    ? Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
                         ),
+                        height: MediaQuery.of(context).size.height * 0.3,
+                        width: MediaQuery.of(context).size.height * 0.3,
+                        child: Image.file(
+                          image!,
+                          fit: BoxFit.cover,
+                        ),
+                      )
+                    : Stack(
+                        children: [
+                          Container(
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              // border: Border.all(
+                              //   color: Pallete.primaryColor,
+                              //   width: 3.5,
+                              // ),
+                            ),
+                            height: MediaQuery.of(context).size.height * 0.3,
+                            width: MediaQuery.of(context).size.width,
+                            child: Image.asset(
+                              'assets/images/profileImagePlaceholder.png',
+                            ),
+                          ),
+                          Positioned(
+                            left: MediaQuery.of(context).size.width * 0.25,
+                            bottom: MediaQuery.of(context).size.height * 0.01,
+                            child: IconButton(
+                              onPressed: selectImage,
+                              icon: Icon(
+                                Icons.add_a_photo,
+                                size: 40,
+                                color: Pallete.primaryColor,
+                              ),
+                            ),
+                          ),
+                        ],
                       ),
-                    ),
-                  ],
-                ),
                 SizedBox(height: 20),
                 ProjectTextfield(text: "Gender", controller: genderController),
                 SizedBox(height: 20),
