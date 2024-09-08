7b10a28de257fa5d5a33e1e6d3c284653acb2aca
Saurav Haldar <143389228+SauravHaldar04@users.noreply.github.com>
2024-09-09 00:38:00 +0530

    Parent remote data source and parent repository

---
+++ lib/features/profile/data/datasources/parent_remote_datasource.dart
@@ -0,0 +1,97 @@
+import 'dart:io';
+
+import 'package:aparna_education/core/error/server_exception.dart';
+import 'package:aparna_education/core/network/download.dart';
+import 'package:aparna_education/features/profile/data/models/parent_model.dart';
+import 'package:aparna_education/features/profile/domain/entities/parent_entity.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+abstract interface class ParentRemoteDatasource {
+  FirebaseFirestore get firestore;
+  FirebaseAuth get firebaseAuth;
+  Future<List<Parent>> getParents();
+  Future<Parent> getParent(String uid);
+  Future<void> addParent({
+    required String firstName,
+    required String middleName,
+    required String lastName,
+    required String phoneNumber,
+    required String address,
+    required String city,
+    required String state,
+    required String country,
+    required String pincode,
+    required String gender,
+    required DateTime dob,
+    required File profilePic,
+    required String occupation,
+  });
+}
+
+class ParentRemoteDatasourceImpl implements ParentRemoteDatasource {
+  @override
+  final FirebaseFirestore firestore;
+  @override
+  final FirebaseAuth firebaseAuth;
+  ParentRemoteDatasourceImpl(this.firestore, this.firebaseAuth);
+
+  @override
+  Future<void> addParent(
+      {required String firstName,
+      required String middleName,
+      required String lastName,
+      required String phoneNumber,
+      required String address,
+      required String city,
+      required String state,
+      required String country,
+      required String pincode,
+      required String gender,
+      required DateTime dob,
+      required File profilePic,
+      required String occupation}) async {
+    try {
+      final imageUrl = await uploadAndGetDownloadUrl('profilePics', profilePic);
+      final uid = firebaseAuth.currentUser!.uid;
+      final parent = ParentModel(
+        uid: uid,
+        email: firebaseAuth.currentUser!.email!,
+        firstName: firstName,
+        middleName: middleName,
+        lastName: lastName,
+        phoneNumber: phoneNumber,
+        address: address,
+        city: city,
+        state: state,
+        country: country,
+        pincode: pincode,
+        occupation: occupation,
+        profilePic: imageUrl,
+        gender: gender,
+        dob: dob,
+      );
+      await firestore.collection('parents').doc(uid).set(parent.toMap());
+      await firestore.collection('users').doc(uid).update({
+        'firstName': firstName,
+        'lastName': lastName,
+        'middleName': middleName,
+        'role': 'parent',
+      });
+    } catch (e) {
+      throw ServerException(message: e.toString());
+    }
+  }
+
+  @override
+  Future<Parent> getParent(String uid) {
+    // TODO: implement getParent
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<List<Parent>> getParents() {
+    // TODO: implement getParents
+    throw UnimplementedError();
+  }
+}
---
+++ lib/features/profile/domain/repositories/parent_repository.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:aparna_education/core/error/failure.dart';
+import 'package:aparna_education/core/success/success.dart';
+import 'package:aparna_education/features/profile/domain/entities/parent_entity.dart';
+import 'package:fpdart/fpdart.dart';
+
+abstract interface class ParentRepository {
+  Future<Either<Failure,Success>> addParent({
+    required String firstName,
+    required String middleName,
+    required String lastName,
+    required String phoneNumber,
+    required String address,
+    required String city,
+    required String state,
+    required String country,
+    required String pincode,
+    required String gender,
+    required DateTime dob,
+    required File profilePic,
+    required String occupation,
+  });
+  Future<Either<Failure,Parent>> getParent(String uid);
+  Future<Either<Failure,List<Parent>>> getParents();
+}
